a4020b883c33a19fe1770f70012217bcdd47d964
illimpassa <illimpassa@gmail.com>
2022-06-30 20:12:48 +0100

    navigation installation 1

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
+import HomeScreen from './screens/HomeScreen'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+
 import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -9,6 +16,16 @@ import {
 const App: () => Node = () => {
   return (
     <SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: 'Welcome' }}
+          />
+          {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   );
 };
---
+++ screens/HomeScreen.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <div>HomeScreen</div>
+  )
+}
